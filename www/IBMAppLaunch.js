b72b31622aaa2a51afcb79f5dd61d1dfa71cab91
Vittal Pai <vittalpai@ymail.com>
2017-12-06 12:00:15 +0530

    Add Initialization API

--- www/IBMAppLaunch.js
+++ www/IBMAppLaunch.js
@@ -1,5 +1,24 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "IBMAppLaunch", "coolMethod", [arg0]);
+var IBMAppLaunch = function () {
+    const IBMAppLaunchString = "AppLaunchPlugin";
+    var success = function (message) {
+        console.log(IBMAppLaunchString + ": Success: " + message);
+    };
+    var failure = function (message) {
+        console.log(IBMAppLaunchString + ": Failure: " + message);
+    };
+
+    /**
+     *  the device on to the IMFPush Notification Server
+     *
+     * @param appRegion
+     * @param appGuid
+     * @param clientSecret
+     */
+    this.initialize = function (appRegion, appGuid, clientSecret) {
+        cordova.exec(success, failure, IBMAppLaunchString, "initialize", [appRegion, appGuid, clientSecret]);
+    };
 };
+
+module.exports = new IBMAppLaunch();
